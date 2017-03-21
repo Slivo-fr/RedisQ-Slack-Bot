a8ef27ffce0cdd8f743955cac1f5e64af698239b
slivo-fr <gchopelet@gmail.com>
2017-03-21 14:48:19 +0100

    Initial commit

---
+++ Killbot.php
@@ -0,0 +1,222 @@
+<?php
+
+
+class Killbot {
+
+    protected $knownShipList = array();
+
+    public function run() {
+
+        $isFeedEmpty = false;
+        $timeout = time() + Settings::$MAX_RUN_TIME;
+
+        while (!$isFeedEmpty && time() < $timeout) {
+
+            $isFeedEmpty = !$this->processKill();
+        }
+    }
+
+    /*
+     * Fetch and process one kill from RedisQ
+     * Return false if the queue is empty, true otherwise
+     */
+    public function processKill() {
+
+        $rawOutput = $this->curlRequest(Settings::$REDISQ_URL);
+
+        $data = json_decode($rawOutput);
+
+        if ($data->{'package'} == null) {
+            return false;
+        }
+
+        $killmail = $data->{'package'}->{'killmail'};
+
+        // Only process kill that match settings entities
+        if ($this->isWatchedKill($killmail)) {
+
+            $zkb = $data->{'package'}->{'zkb'};
+            $jsonKill = new stdClass();
+            $jsonAttachements = new stdClass();
+
+            $killer = null;
+            $noVictim = false;
+            $attackerCount = $killmail->{'attackerCount'};
+            $killId = $killmail->{'killID'};
+
+            var_dump($killId);
+
+            // Looking up final blow
+            foreach ($killmail->{'attackers'} as $attacker) {
+                if ($attacker->{'finalBlow'} == true) {
+                    $killer = $attacker;
+                }
+            }
+
+            // Handling NPC kills
+            if (!isset($killer->{'character'}->{'name'})) {
+                $killerName = $killer->{'shipType'}->{'name'};
+            } else {
+                $killerName = $killer->{'character'}->{'name'};
+            }
+
+            // Handling case with no pilots victims
+            $victim = $killmail->{'victim'};
+            if (!isset($victim->{'character'}->{'name'})) {
+                $noVictim = true;
+                $victimName = $victim->{'corporation'}->{'name'};
+            } else {
+                $victimName = $victim->{'character'}->{'name'};
+            }
+
+            $victimShipId = $victim->{'shipType'}->{'id'};
+            $victimShipName = $this->getShipName($victimShipId);
+
+            $isVictimCorpWatched = in_array($victim->{'corporation'}->{'id'}, Settings::$WATCHED_ENTITIES['corporations']);
+            $isVictimAllianceWatched =
+                isset($victim->{'alliance'}) &&
+                in_array($victim->{'alliance'}->{'id'}, Settings::$WATCHED_ENTITIES['alliances']);
+
+            // Dissociating kill and loss
+            if ($isVictimAllianceWatched || $isVictimCorpWatched) {
+                $killerCorpName = $killer->{'corporation'}->{'name'};
+                $jsonKill->fallback = "$victimName's $victimShipName got killed by $killerName ($killerCorpName)";
+                $jsonKill->color = 'danger';
+            } else {
+                $victimCorpName = $victim->{'corporation'}->{'name'};
+
+                if($noVictim) {
+                    $jsonKill->fallback = "$killerName killed $victimName's $victimShipName";
+                } else {
+                    $jsonKill->fallback = "$killerName killed $victimName's $victimShipName ($victimCorpName)";
+                }
+
+                $jsonKill->color = 'good';
+            }
+
+
+            /*
+             * Formatting data for slack
+             */
+
+            $jsonKill->title = $jsonKill->fallback;
+            $jsonKill->title_link = "https://zkillboard.com/kill/$killId/";
+            $jsonKill->thumb_url = "https://imageserver.eveonline.com/Render/".$victimShipId."_64.png";
+
+            $killValue = number_format($zkb->{'totalValue'}, 2, ',', ' ');
+
+            $jsonKillValue = new stdClass();
+            $jsonKillValue->title = 'Value';
+            $jsonKillValue->value = $killValue . ' ISK';
+            $jsonKillValue->short = 'true';
+
+            $jsonTotalAttackers = new stdClass();
+            $jsonTotalAttackers->title = 'Pilots involved';
+            $jsonTotalAttackers->value = $attackerCount;
+            $jsonTotalAttackers->short = 'true';
+
+            $jsonKill->fields = [$jsonKillValue, $jsonTotalAttackers];
+            $jsonAttachements->attachments = [$jsonKill];
+
+            $this->pushToSlack($jsonAttachements);
+        }
+
+
+        return true;
+    }
+
+    private function isWatchedKill($killmail) {
+
+        $isVictimWatched = $this->isVictimWatched($killmail);
+        $isAttackerWatched = $this->isAttackerWatched($killmail);
+
+        return $isVictimWatched || $isAttackerWatched;
+    }
+
+    private function isVictimWatched($killmail) {
+
+        $victim = $killmail->{'victim'};
+
+        if (isset($victim->{'corporation'}->{'id'})) {
+            if (in_array($victim->{'corporation'}->{'id'}, Settings::$WATCHED_ENTITIES['corporations'])){
+                return true;
+            }
+        }
+
+        if (isset($victim->{'alliance'}->{'id'})) {
+            if (in_array($victim->{'alliance'}->{'id'}, Settings::$WATCHED_ENTITIES['alliances'])){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function isAttackerWatched($killmail) {
+
+        $attackers = $killmail->{'attackers'};
+
+        foreach ($attackers as $attacker) {
+
+            if (isset($attacker->{'corporation'}->{'id'})) {
+                if (in_array($attacker->{'corporation'}->{'id'}, Settings::$WATCHED_ENTITIES['corporations'])){
+                    return true;
+                }
+            }
+
+            if (isset($attacker->{'alliance'}->{'id'})) {
+                if (in_array($attacker->{'alliance'}->{'id'}, Settings::$WATCHED_ENTITIES['alliances'])){
+                    return true;
+                }
+            }
+        }
+    }
+
+
+    private function getShipName($victimShipId) {
+
+        if (in_array($victimShipId, $this->knownShipList)) {
+            return $this->knownShipList[$victimShipId];
+        }
+
+        $json = $this->curlRequest("https://crest-tq.eveonline.com/inventory/types/$victimShipId/");
+        $data = json_decode($json);
+
+        $shipName = $data->{'name'};
+        $this->knownShipList[$victimShipId] = $shipName;
+
+        return $shipName;
+    }
+
+    private function curlRequest ($url) {
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_USERAGENT, Settings::$HTTP_HEADER);
+
+        $output = curl_exec($ch);
+        curl_close($ch);
+
+        return $output;
+    }
+
+    private function pushToSlack($killData) {
+
+        $data = "payload=" .json_encode($killData);
+
+        $ch = curl_init(Settings::$SLACK_HOOK);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+        curl_exec($ch);
+        curl_close($ch);
+    }
+
+}
---
+++ Settings.php
@@ -0,0 +1,37 @@
+<?php
+
+class Settings {
+
+    /*******************************************************************************************************************
+     * Basic configuration
+     * Should be enough to get your bot running.
+     ******************************************************************************************************************/
+
+    // Slack webhook URL
+    public static $SLACK_HOOK   = 'https://hooks.slack.com/services/ABC/DEF/GHI';
+
+    // 4 minutes default max run time
+    public static $MAX_RUN_TIME = 4 * 60;
+
+    // Entities you want to display killmails
+    public static $WATCHED_ENTITIES = [
+        'corporations' => [
+            123456,
+            1324567
+        ],
+        'alliances' => [
+            13245678
+        ]
+    ];
+
+    /*******************************************************************************************************************
+     * Advanced configuration
+     * Do not edit until you know what you are doing
+     ******************************************************************************************************************/
+
+    // HTTP header sent with each bot request
+    public static $HTTP_HEADER  = 'RedisQ-Slack-Bot https://github.com/Slivo-fr/RedisQ-Slack-Bot';
+
+    // URL to redisQ
+    public static $REDISQ_URL = 'https://redisq.zkillboard.com/listen.php?ttw=1';
+}
